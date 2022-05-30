1428aa8fd38cc45154a22a792ccf47a20feb0723
poojadugupta <poojadugupta@gmail.com>
2022-05-30 16:01:07 +0530

    color changed to pink

--- lib/nomad/favorite.dart
+++ lib/nomad/favorite.dart
@@ -11,7 +11,7 @@ class _FavoriteState extends State<Favorite> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
+      color: Colors.pink,
       child: const Text("favorite", style: TextStyle(fontSize: 50)),
     );
   }
